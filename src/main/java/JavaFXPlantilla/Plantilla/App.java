06fa23ce6109b5582fd78a4a8c686b081da404d4
ivyygh <ivanrp98@gmail.com>
2020-10-02 10:49:55 +0100

    Initial commit

---
+++ src/main/java/JavaFXPlantilla/Plantilla/App.java
@@ -0,0 +1,13 @@
+package JavaFXPlantilla.Plantilla;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
